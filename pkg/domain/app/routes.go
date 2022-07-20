e8a5de9eed880e54de686ef7db5d85ab2142ebe9
Greg T. Wallace <greg@gregtwallace.com>
2022-07-20 10:45:04 -0400

    app: rearrange routes no logic changes

--- pkg/domain/app/routes.go
+++ pkg/domain/app/routes.go
@@ -16,30 +16,33 @@ func (app *Application) Routes() http.Handler {
 
 	// private_keys
 	app.makeHandle(http.MethodGet, "/api/v1/privatekeys", app.keys.GetAllKeys)
+	app.makeHandle(http.MethodGet, "/api/v1/privatekeys/:id", app.keys.GetOneKey)
+
 	app.makeHandle(http.MethodPost, "/api/v1/privatekeys", app.keys.PostNewKey)
 
-	app.makeHandle(http.MethodGet, "/api/v1/privatekeys/:id", app.keys.GetOneKey)
 	app.makeHandle(http.MethodPut, "/api/v1/privatekeys/:id", app.keys.PutNameDescKey)
 
 	app.makeHandle(http.MethodDelete, "/api/v1/privatekeys/:id", app.keys.DeleteKey)
 
 	// acme_accounts
 	app.makeHandle(http.MethodGet, "/api/v1/acmeaccounts", app.accounts.GetAllAccounts)
+	app.makeHandle(http.MethodGet, "/api/v1/acmeaccounts/:id", app.accounts.GetOneAccount)
+
 	app.makeHandle(http.MethodPost, "/api/v1/acmeaccounts", app.accounts.PostNewAccount)
 
-	app.makeHandle(http.MethodGet, "/api/v1/acmeaccounts/:id", app.accounts.GetOneAccount)
 	app.makeHandle(http.MethodPut, "/api/v1/acmeaccounts/:id", app.accounts.PutNameDescAccount)
+	app.makeHandle(http.MethodPut, "/api/v1/acmeaccounts/:id/email", app.accounts.ChangeEmail)
+
 	app.makeHandle(http.MethodPost, "/api/v1/acmeaccounts/:id/new-account", app.accounts.NewAcmeAccount)
 	app.makeHandle(http.MethodPost, "/api/v1/acmeaccounts/:id/deactivate", app.accounts.Deactivate)
-	app.makeHandle(http.MethodPut, "/api/v1/acmeaccounts/:id/email", app.accounts.ChangeEmail)
 
 	app.makeHandle(http.MethodDelete, "/api/v1/acmeaccounts/:id", app.accounts.DeleteAccount)
 
 	// certificates
 	app.makeHandle(http.MethodGet, "/api/v1/certificates", app.certificates.GetAllCerts)
-	app.makeHandle(http.MethodPost, "/api/v1/certificates", app.certificates.PostNewCert)
-
 	app.makeHandle(http.MethodGet, "/api/v1/certificates/:id", app.certificates.GetOneCert)
+
+	app.makeHandle(http.MethodPost, "/api/v1/certificates", app.certificates.PostNewCert)
 
 	// invalid route
 	app.router.NotFound = app.makeHandler(app.notFoundHandler)
