fe828f8121d67b242bfa9b5dbbb25932edf1229e
Greg T. Wallace <greg@gregtwallace.com>
2022-08-03 16:34:21 -0400

    challenges: more temp tweaks for testing testing orders / auths need some minor updates here to facilitate

--- pkg/challenges/solver.go
+++ pkg/challenges/solver.go
@@ -1,9 +1,12 @@
 package challenges
 
 import (
+	"errors"
 	"legocerthub-backend/pkg/acme"
 	"time"
 )
+
+var ErrChallengeNotValid = errors.New("challenge is not valid")
 
 // Solve accepts the array of challengeUrls from an authorization and solves the specific challenge
 // specified by the method.
@@ -17,6 +20,8 @@ func (service *Service) Solve(challenges []acme.Challenge, method Method, key ac
 
 		service.http01.AddToken(challenges[0].Token, key)
 
+		time.Sleep(10 * time.Second) // TODO: Remove - artificial delay for testing duplicative stuff
+
 		chall, err = service.acmeStaging.ValidateChallenge(challenges[0].Url, key)
 
 		time.Sleep(20 * time.Second)
@@ -28,6 +33,10 @@ func (service *Service) Solve(challenges []acme.Challenge, method Method, key ac
 		// TODO: Prod
 	}
 	//
+
+	if chall.Status != "valid" {
+		return ErrChallengeNotValid
+	}
 
 	return nil
 
