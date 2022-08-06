e763246ab45063d0edeee26f9a5be2d4860eeb39
Greg T. Wallace <greg@gregtwallace.com>
2022-08-06 16:37:10 -0400

    storage: update key delete logic to avoid accidental key deletion if still in use

--- pkg/storage/sqlite/keys_delete.go
+++ pkg/storage/sqlite/keys_delete.go
@@ -43,10 +43,42 @@ func (store *Storage) KeyInUse(id int) (inUse bool, err error) {
 
 	// check not in use in certs
 	// if scan in succeeds, record exists in certificates
+	// this confirms a cert isn't trying to use this key in future orders
 	query = `
 	SELECT id
 	FROM certificates
 	WHERE private_key_id = $1
+	`
+
+	row = store.Db.QueryRowContext(ctx, query, id)
+	temp = -2
+	row.Scan(&temp)
+	if temp != -2 {
+		return true, nil
+	}
+
+	// check not in use by valid order on an existing cert_id with longest dated expiration
+	// query groups valid orders by cert_id and then returns a result if the
+	// order has the max valid_to for a particular key (the one being deleted)
+	// This query is used to confirm the key isn't needed for any of the currently hosted cert records.
+	// Needed because cert could be rotating key (cert record has a different key id now) and
+	// don't want to delete key that may still be needed by pre-rotation cert
+	query = `
+	SELECT
+		certificate_id
+	FROM
+		acme_orders
+	WHERE 
+		status = "valid"
+		AND
+		certificate_id not null
+	GROUP BY
+		certificate_id
+	HAVING
+		MAX(valid_to)
+		AND
+		finalized_key_id = $1
+	
 	`
 
 	row = store.Db.QueryRowContext(ctx, query, id)
@@ -64,71 +96,24 @@ func (store *Storage) DeleteKey(id int) error {
 	ctx, cancel := context.WithTimeout(context.Background(), store.Timeout)
 	defer cancel()
 
-	tx, err := store.Db.BeginTx(ctx, nil)
+	// check that delete is safe
+	inUse, err := store.KeyInUse(id)
 	if err != nil {
 		return err
 	}
-	defer tx.Rollback()
-
-	// check key exists
-	// if scan in succeeds, key exists
-	query := `
-	SELECT id
-	FROM private_keys
-	WHERE id = $1
-	`
-
-	row := tx.QueryRowContext(ctx, query, id)
-	temp := -2
-	row.Scan(&temp)
-	if temp == -2 {
-		return storage.ErrNoRecord
-	}
-
-	// check not in use in accounts
-	// if scan in succeeds, record exists in acme_accounts
-	query = `
-	SELECT id
-	FROM acme_accounts
-	WHERE private_key_id = $1
-	`
-
-	row = tx.QueryRowContext(ctx, query, id)
-	temp = -2
-	row.Scan(&temp)
-	if temp != -2 {
-		return storage.ErrInUse
-	}
-
-	// check not in use in certs
-	// if scan in succeeds, record exists in certificates
-	query = `
-	SELECT id
-	FROM certificates
-	WHERE private_key_id = $1
-	`
-
-	row = tx.QueryRowContext(ctx, query, id)
-	temp = -2
-	row.Scan(&temp)
-	if temp != -2 {
+	if inUse {
 		return storage.ErrInUse
 	}
 
 	// delete
-	query = `
+	query := `
 	DELETE FROM
 		private_keys
 	WHERE
 		id = $1
 	`
 
-	_, err = tx.ExecContext(ctx, query, id)
-	if err != nil {
-		return err
-	}
-
-	err = tx.Commit()
+	_, err = store.Db.ExecContext(ctx, query, id)
 	if err != nil {
 		return err
 	}
