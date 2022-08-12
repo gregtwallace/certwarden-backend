e046e0e76b55aa50997b70cccbe1b67a3a44fb99
Greg T. Wallace <greg@gregtwallace.com>
2022-08-11 20:03:22 -0400

    version: 0.1.0 fully functional ACME management still low version due to needing: login, https, dashboard, settings, etc.

--- pkg/domain/app/configure.go
+++ pkg/domain/app/configure.go
@@ -16,7 +16,7 @@ import (
 )
 
 // application version
-const appVersion = "0.0.1"
+const appVersion = "0.1.0"
 
 // Directory URLs for Let's Encrypt
 const acmeProdUrl string = "https://acme-v02.api.letsencrypt.org/directory"
