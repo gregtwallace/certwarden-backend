143791a83cc77a10fcf53737567afe03d646d911
Greg T. Wallace <greg@gregtwallace.com>
2022-08-16 11:32:11 -0400

    auth: keyFunc signature enforcement

--- pkg/domain/app/auth/tokens.go
+++ pkg/domain/app/auth/tokens.go
@@ -12,12 +12,31 @@ var accessJwtSecret = []byte("17842911225de55706cb6e417418c7a0d21c9ccaf1c4ec271e
 var refreshJwtSecret = []byte("de0bce3589c282acc4e917eb1af6f85521624681e7dded2542004d26d1f5e87b")
 
 // expiration times
-const accessTokenExpiration = 5 * time.Minute
-const refreshTokenExpiration = 30 * time.Minute
+// const accessTokenExpiration = 2 * time.Minute
+// const refreshTokenExpiration = 30 * time.Minute
+
+const accessTokenExpiration = 10 * time.Second
+const refreshTokenExpiration = 30 * time.Second
 
 // token types
 type accessToken string
 type refreshToken string
+
+// jwt signature method
+var tokenSignatureMethod = jwt.SigningMethodHS256
+
+// keyFunc provides the jwt.KeyFunc
+func makeKeyFunc(secret []byte) func(*jwt.Token) (interface{}, error) {
+	return func(token *jwt.Token) (interface{}, error) {
+		// confirm signature is of proper type
+		// see: https://auth0.com/blog/critical-vulnerabilities-in-json-web-token-libraries/
+		if token.Method != tokenSignatureMethod {
+			return nil, jwt.ErrSignatureInvalid
+		}
+
+		return secret, nil
+	}
+}
 
 // create access token
 func createAccessToken(username string) (accessToken, error) {
@@ -31,7 +50,7 @@ func createAccessToken(username string) (accessToken, error) {
 	}
 
 	// create token and then signed token string
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	token := jwt.NewWithClaims(tokenSignatureMethod, claims)
 	tokenString, err := token.SignedString(accessJwtSecret)
 	if err != nil {
 		return "", err
@@ -53,7 +72,7 @@ func createRefreshToken(username string) (refreshToken, error) {
 	}
 
 	// create token and then signed token string
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	token := jwt.NewWithClaims(tokenSignatureMethod, claims)
 	refreshString, err := token.SignedString(refreshJwtSecret)
 	if err != nil {
 		return "", err
@@ -66,9 +85,7 @@ func createRefreshToken(username string) (refreshToken, error) {
 // an error is returned if there is any issue (e.g. token not valid)
 func (tokenString *accessToken) valid() (claims jwt.MapClaims, err error) {
 	// parse and validate token
-	token, err := jwt.Parse(string(*tokenString), func(token *jwt.Token) (interface{}, error) {
-		return accessJwtSecret, nil
-	})
+	token, err := jwt.Parse(string(*tokenString), makeKeyFunc(accessJwtSecret))
 	if err != nil {
 		return nil, output.ErrUnauthorized
 	}
@@ -91,9 +108,7 @@ func (tokenString *accessToken) valid() (claims jwt.MapClaims, err error) {
 // issue (e.g. token not valid)
 func (tokenString *refreshToken) valid() (claims jwt.MapClaims, err error) {
 	// parse and validate token
-	token, err := jwt.Parse(string(*tokenString), func(token *jwt.Token) (interface{}, error) {
-		return refreshJwtSecret, nil
-	})
+	token, err := jwt.Parse(string(*tokenString), makeKeyFunc(refreshJwtSecret))
 	if err != nil {
 		return nil, output.ErrUnauthorized
 	}
