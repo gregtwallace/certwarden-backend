497b7e38a48dfd5a498c27d0da315f487db565fb
Greg T. Wallace <greg@gregtwallace.com>
2022-08-28 10:18:19 -0400

    frontend: move env to own file

--- pkg/domain/app/frontend/run.go
+++ pkg/domain/app/frontend/run.go
@@ -3,12 +3,10 @@ package frontend
 import (
 	"fmt"
 	"net/http"
-	"os"
 	"time"
 )
 
 const buildDir = "./frontend_build"
-const envFile = buildDir + "/env.js"
 
 // runFrontend launches a web server to host the frontend
 func (service *Service) run() error {
@@ -63,33 +61,6 @@ func (service *Service) run() error {
 		go func() {
 			service.logger.Panic(srv.ListenAndServe())
 		}()
-	}
-
-	return nil
-}
-
-// setFrontendEnv creates the env.js file in the frontend build. This is used
-// to set variables at server run time
-func setFrontendEnv(apiUrl string) error {
-	// remove any old environment
-	_ = os.Remove(envFile)
-
-	// content of new environment file
-	envFileContent := `
-	window.env = {
-		API_URL: '` + apiUrl + `',
-	};
-	`
-
-	file, err := os.Create(envFile)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	_, err = file.Write([]byte(envFileContent))
-	if err != nil {
-		return err
 	}
 
 	return nil
