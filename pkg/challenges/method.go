ac58d99054063b0f792342b5e34aaa1b93a7d8e3
Greg T. Wallace <greg@gregtwallace.com>
2022-09-19 10:13:52 -0400

    challenges: refactor code for more sanity

--- pkg/challenges/method.go
+++ pkg/challenges/method.go
@@ -20,75 +20,29 @@ const (
 	dns01Script
 )
 
-// Define MethodDetails which contains details about the defined
-// methods.
-type challMethodDetails struct {
-	method Method             `json:"-"`
-	Value  string             `json:"value"`
-	Name   string             `json:"name"`
-	Type   acme.ChallengeType `json:"type"`
-}
-
-var methodDetails = []challMethodDetails{
-	{
-		// serve the http record from an internal http server
-		method: http01Internal,
-		Value:  "http-01-internal",
-		Name:   "HTTP (Self Served)",
-		Type:   acme.Http01,
-	},
-	// TODO: Implement DNS
-	// {
-	// 	// call external scripts to create and delete dns records
-	// 	Method:        dns01Script,
-	// 	Value:         "dns-01-script",
-	// 	Name:          "DNS-01 (Manual Script)",
-	// 	ChallengeType: acme.Dns01,
-	// },
-}
-
 // Method custom JSON Marshal (turns the Method into MethodDetails for output)
 func (method *Method) MarshalJSON() (data []byte, err error) {
+	// get method details
+	details := method.details()
+
+	// put the exportable details into an exportable struct
+	output := struct {
+		Value         string             `json:"value"`
+		Name          string             `json:"name"`
+		ChallengeType acme.ChallengeType `json:"type"`
+	}{
+		Value:         details.value,
+		Name:          details.name,
+		ChallengeType: details.challengeType,
+	}
+
 	// return details marshalled
-	return json.Marshal(method.details())
+	return json.Marshal(output)
 }
 
 // custom UnmarshalJSON not needed at present
 
-// ListOfMethods() returns a slice of challenge methods
-func ListOfMethods() (methods []Method) {
-	// loop through details to make slice of methods
-	for i := range methodDetails {
-		methods = append(methods, methodDetails[i].method)
-	}
-
-	return methods
-}
-
-// MethodByValue returns a challenge method based on its Value.
-// If a method isn't found, UnknownMethod is returned.
-func MethodByValue(value string) Method {
-	for i := range methodDetails {
-		if value == methodDetails[i].Value {
-			return methodDetails[i].method
-		}
-	}
-
-	return UnknownMethod
-}
-
-// Type returns the Challenge Type for the Method.
-func (method Method) challType() acme.ChallengeType {
-	for i := range methodDetails {
-		if method == methodDetails[i].method {
-			return methodDetails[i].Type
-		}
-	}
-
-	return acme.UnknownChallengeType
-}
-
-// Type returns the full details for the Method.
+// details returns the full details for the Method.
 func (method Method) details() challMethodDetails {
 	for i := range methodDetails {
 		if method == methodDetails[i].method {
@@ -100,8 +54,13 @@ func (method Method) details() challMethodDetails {
 	return challMethodDetails{}
 }
 
+// Type returns the Challenge Type for the Method.
+func (method Method) challengeType() acme.ChallengeType {
+	return method.details().challengeType
+}
+
 // validationResource creates the resource name and content that are required
 // to succesfully validate an ACME Challenge.
 func (method Method) validationResource(identifier acme.Identifier, key acme.AccountKey, token string) (name string, content string, err error) {
-	return method.challType().ValidationResource(identifier, key, token)
+	return method.challengeType().ValidationResource(identifier, key, token)
 }
---
+++ pkg/challenges/methods.go
@@ -0,0 +1,52 @@
+package challenges
+
+import "legocerthub-backend/pkg/acme"
+
+// Define MethodDetails which contains details about the defined
+// methods.
+type challMethodDetails struct {
+	method        Method
+	value         string
+	name          string
+	challengeType acme.ChallengeType
+}
+
+var methodDetails = []challMethodDetails{
+	{
+		// serve the http record from an internal http server
+		method:        http01Internal,
+		value:         "http-01-internal",
+		name:          "HTTP (Self Served)",
+		challengeType: acme.Http01,
+	},
+	// TODO: Implement DNS
+	// {
+	// 	// call external scripts to create and delete dns records
+	// 	Method:        dns01Script,
+	// 	Value:         "dns-01-script",
+	// 	Name:          "DNS-01 (Manual Script)",
+	// 	ChallengeType: acme.Dns01,
+	// },
+}
+
+// ListOfMethods() returns a slice of challenge methods
+func ListOfMethods() (methods []Method) {
+	// loop through details to make slice of methods
+	for i := range methodDetails {
+		methods = append(methods, methodDetails[i].method)
+	}
+
+	return methods
+}
+
+// MethodByValue returns a challenge method based on its Value.
+// If a method isn't found, UnknownMethod is returned.
+func MethodByValue(value string) Method {
+	for i := range methodDetails {
+		if value == methodDetails[i].value {
+			return methodDetails[i].method
+		}
+	}
+
+	return UnknownMethod
+}
