0cdb6425fc8bf8ad4d09d59a82740202d2bc5894
Greg T. Wallace <greg@gregtwallace.com>
2022-09-22 11:56:17 -0400

    sqlite: simplify post new key

--- pkg/storage/sqlite/keys_post.go
+++ pkg/storage/sqlite/keys_post.go
@@ -5,27 +5,8 @@ import (
 	"legocerthub-backend/pkg/domain/private_keys"
 )
 
-// newKeyToDb translates a KeyExtended into a db object for storage
-func newKeyToDb(newKey private_keys.NewPayload) keyDbExtended {
-	var dbObj keyDbExtended
-
-	dbObj.name = *newKey.Name
-	dbObj.description = *newKey.Description
-	dbObj.algorithmValue = *newKey.AlgorithmValue
-	dbObj.pem = *newKey.PemContent
-	dbObj.apiKey = newKey.ApiKey
-	dbObj.apiKeyViaUrl = newKey.ApiKeyViaUrl
-	dbObj.createdAt = newKey.CreatedAt
-	dbObj.updatedAt = newKey.UpdatedAt
-
-	return dbObj
-}
-
 // PostNewKey saves the KeyExtended to the db as a new key
 func (store *Storage) PostNewKey(payload private_keys.NewPayload) (id int, err error) {
-	// load payload fields into db struct
-	keyDb := newKeyToDb(payload)
-
 	// database action
 	ctx, cancel := context.WithTimeout(context.Background(), store.Timeout)
 	defer cancel()
@@ -38,14 +19,14 @@ func (store *Storage) PostNewKey(payload private_keys.NewPayload) (id int, err e
 
 	// insert and scan the new id
 	err = store.Db.QueryRowContext(ctx, query,
-		keyDb.name,
-		keyDb.description,
-		keyDb.algorithmValue,
-		keyDb.pem,
-		keyDb.apiKey,
-		keyDb.apiKeyViaUrl,
-		keyDb.createdAt,
-		keyDb.updatedAt,
+		payload.Name,
+		payload.Description,
+		payload.AlgorithmValue,
+		payload.PemContent,
+		payload.ApiKey,
+		payload.ApiKeyViaUrl,
+		payload.CreatedAt,
+		payload.UpdatedAt,
 	).Scan(&id)
 
 	if err != nil {
