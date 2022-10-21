dd7d5ba8836f539a5aecf7936e205704553b026b
Greg T. Wallace <greg@gregtwallace.com>
2022-10-21 15:08:15 -0400

    storage: fix empty cjs

--- pkg/storage/sqlite/commajoinedstrings.go
+++ pkg/storage/sqlite/commajoinedstrings.go
@@ -10,10 +10,18 @@ type commaJoinedStrings string
 
 // transform CJS into string slice
 func (cjs commaJoinedStrings) toSlice() []string {
+	if cjs == "" {
+		return []string{}
+	}
+
 	return strings.Split(string(cjs), ",")
 }
 
 // makeCommaJoinedString creates a CJS from a slice of strings
 func makeCommaJoinedString(stringSlice []string) commaJoinedStrings {
+	if len(stringSlice) == 0 {
+		return ""
+	}
+
 	return commaJoinedStrings(strings.Join(stringSlice, ","))
 }
