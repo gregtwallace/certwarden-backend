297599c66afc2ebce8294d1ff00ee0a9cc6bd4c4
Greg T. Wallace <greg@gregtwallace.com>
2022-11-03 19:11:43 -0400

    v.0.4.0

--- pkg/domain/app/app.go
+++ pkg/domain/app/app.go
@@ -20,7 +20,7 @@ import (
 )
 
 // application version
-const appVersion = "0.3.2"
+const appVersion = "0.4.0"
 
 // Directory URLs for Let's Encrypt
 const acmeProdUrl string = "https://acme-v02.api.letsencrypt.org/directory"
