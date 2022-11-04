dca76454cb1efe3b3f0d1e79b2308b0d0b6f07b5
Greg T. Wallace <greg@gregtwallace.com>
2022-11-04 17:58:26 -0400

    http01internal: minor handler cleanup

--- pkg/challenges/providers/http01internal/handlers.go
+++ pkg/challenges/providers/http01internal/handlers.go
@@ -10,28 +10,38 @@ import (
 // token exists in this service's tokens, the expected token content is sent back to
 // the client. If the token is not in the service's tokens, a 404 reply is sent.
 func (service *Service) challengeHandler(w http.ResponseWriter, r *http.Request) {
+	var keyAuth string
+	var exists bool
+	var err error
+
 	// token from the client request
 	token := httprouter.ParamsFromContext(r.Context()).ByName("token")
 
-	// check if token is in served tokens
+	// lock tokens for reading
 	service.mu.RLock()
 	defer service.mu.RUnlock()
 
-	if keyAuth, exists := service.tokens[token]; exists {
-		service.logger.Debugf("writing challenge token to http-01 client: %s", token)
+	// check token existence, if no token, write error 404 and return
+	if keyAuth, exists = service.tokens[token]; !exists {
+		service.logger.Debugf("http-01 challenge token not found: %s", token)
 
-		w.Header().Set("Content-Type", "application/octet-stream")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(keyAuth))
+		w.WriteHeader(http.StatusNotFound)
+		_, err = w.Write([]byte("404 page not found"))
+		if err != nil {
+			service.logger.Errorf("failed to write http-01 404 error: %s", err)
+		}
 
 		// done / exit
 		return
 	}
 
-	// if token was not found, 404
-	service.logger.Debugf("http-01 challenge token not found: %s", token)
+	// token was found, write it
+	service.logger.Debugf("writing challenge token to http-01 client: %s", token)
 
-	w.WriteHeader(http.StatusNotFound)
-	w.Write([]byte("404 page not found"))
-
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.WriteHeader(http.StatusOK)
+	_, err = w.Write([]byte(keyAuth))
+	if err != nil {
+		service.logger.Errorf("failed to write http-01 token: %s", err)
+	}
 }
