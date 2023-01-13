3067ffa130b2771a6e071c27007fa0e5277aae49
Greg T. Wallace <greg@gregtwallace.com>
2023-01-13 17:21:19 -0500

    scripts: linux install

---
+++ scripts/linux/install.sh
@@ -0,0 +1,44 @@
+#/bin/bash
+
+# install path and username
+lego_path=/opt/legocerthub
+lego_user=legocerthub
+
+echo "Enter the ip or hostname for LeGo. This is required to access LeGo"
+echo "from a remote host. Leaving blank or setting to localhost will restrict"
+echo "access to just localhost and will need to be updated in config.yaml later"
+read -p 'Hostname [localhost]: ' hostname
+
+hostname=${hostname:-localhost}
+
+# Check for root
+if [ "$(id -u)" -ne 0 ]; then echo "Please run as root." >&2; exit 1; fi
+
+# create user to run app
+useradd -r -s /bin/false $lego_user
+
+# allow unprivileged binding to port 80
+sysctl -w net.ipv4.ip_unprivileged_port_start=80
+
+# copy all files to install path
+mkdir $lego_path
+cp -R ../* $lego_path
+
+# make config with hostname
+printf "hostname: '$hostname'" > $lego_path/config.yaml
+
+# permissions
+chown $lego_user:$lego_user $lego_path -R
+find $lego_path -type d -exec chmod 755 {} \;
+find $lego_path -type f -exec chmod 640 {} \;
+chmod 750 $lego_path/lego-linux-*
+chmod 750 $lego_path/scripts/*.sh
+
+# allow binding to low port numbers
+# setcap CAP_NET_BIND_SERVICE=+eip /opt/legocerthub/lego-linux-*
+
+# install and start service
+cp ./legocerthub.service /etc/systemd/system/legocerthub.service
+systemctl daemon-reload
+systemctl enable legocerthub
+systemctl start legocerthub
