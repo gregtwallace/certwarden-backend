70ce64743ea379592c8a4fa23690b7c1bfd155a4
Greg T. Wallace <greg@gregtwallace.com>
2023-06-30 19:16:31 -0400

    cloudflare: remove need for zone_names

--- pkg/challenges/providers/dns01cloudflare/configure.go
+++ pkg/challenges/providers/dns01cloudflare/configure.go
@@ -2,13 +2,10 @@ package dns01cloudflare
 
 import (
 	"context"
-	"errors"
 	"legocerthub-backend/pkg/datatypes"
 
 	"github.com/cloudflare/cloudflare-go"
 )
-
-var ErrDuplicateZone = errors.New("duplicate domain (zone) configuration found")
 
 // Configuration options
 type Config struct {
@@ -17,9 +14,10 @@ type Config struct {
 		Email        string `yaml:"email"`
 		GlobalApiKey string `yaml:"global_api_key"`
 	} `yaml:"accounts"`
+	// TODO: Simplify this to not be nested, however, config version number will need
+	// to change as this will break configs.
 	ApiTokens []struct {
-		APIToken  string   `yaml:"api_token"`
-		ZoneNames []string `yaml:"zone_names"`
+		APIToken string `yaml:"api_token"`
 	} `yaml:"api_tokens"`
 }
 
@@ -30,10 +28,27 @@ func (service *Service) configureCloudflareAPI(config *Config) error {
 	// make map to store known domain zones
 	service.knownDomainZones = datatypes.NewSafeMap()
 
+	// configure accounts
+	// create API objects for each account and map all known zones
+	for i := range config.Accounts {
+		// make api for account
+		apiInstance, err := cloudflare.New(config.Accounts[i].GlobalApiKey, config.Accounts[i].Email)
+		if err != nil {
+			service.logger.Error(err)
+			return err
+		}
+
+		// add zones from api
+		err = service.addZonesFromApiInstance(apiInstance)
+		if err != nil {
+			return err
+		}
+	}
+	// configure accounts - END
+
 	// configure domains specified by tokens
 	// for each token add the domain -> api mappings to known domains
 	for i := range config.ApiTokens {
-
 		// make api for the token
 		apiInstance, err := cloudflare.NewWithAPIToken(config.ApiTokens[i].APIToken)
 		if err != nil {
@@ -41,60 +56,36 @@ func (service *Service) configureCloudflareAPI(config *Config) error {
 			return err
 		}
 
-		// add each domain name to known, specify the associated api / ZoneID
-		// and confirm the config is valid for the zone
-		for j := range config.ApiTokens[i].ZoneNames {
-			// test the api token by getting zone ID for the name (domain)
-			zoneID, err := apiInstance.ZoneIDByName(config.ApiTokens[i].ZoneNames[j])
-			if err != nil {
-				service.logger.Error(err)
-				return err
-			}
-
-			// add the zone to the known list
-			z := zone{
-				id:  zoneID,
-				api: apiInstance,
-			}
-			exists, _ := service.knownDomainZones.Add(config.ApiTokens[i].ZoneNames[j], z)
-			// if the same domain/zone is configured more than once, error
-			if exists {
-				err = ErrDuplicateZone
-				service.logger.Error(err)
-				return err
-			}
+		// add zones from api
+		err = service.addZonesFromApiInstance(apiInstance)
+		if err != nil {
+			return err
 		}
 	}
 	// configure domains specified by tokens - END
 
-	// configure accounts
-	// create API objects for each account and map all known zones
-	for i := range config.Accounts {
-		// configure API
-		apiInstance, err := cloudflare.New(config.Accounts[i].GlobalApiKey, config.Accounts[i].Email)
-		if err != nil {
-			service.logger.Error(err)
-			return err
-		}
+	return nil
+}
 
-		// fetch list of zones
-		zoneList, err := apiInstance.ListZones(context.Background())
-		if err != nil {
-			service.logger.Error(err)
-			return err
-		}
-
-		// add all to the known zone list
-		for j := range zoneList {
-			z := zone{
-				id:  zoneList[j].ID,
-				api: apiInstance,
-			}
-			_, _ = service.knownDomainZones.Add(zoneList[j].Name, z)
-			// no error if exists, allow overlap with tokens
-		}
+// addZonesFromApiInstance fetches available zones from a cloudflare API and
+// then adds those zones to the Service available list
+func (service *Service) addZonesFromApiInstance(cfApi *cloudflare.API) error {
+	// fetch list of zones
+	zoneList, err := cfApi.ListZones(context.Background())
+	if err != nil {
+		service.logger.Error(err)
+		return err
 	}
-	// configure accounts - END
+
+	// add all to the known zone list
+	for j := range zoneList {
+		z := zone{
+			id:  zoneList[j].ID,
+			api: cfApi,
+		}
+		_, _ = service.knownDomainZones.Add(zoneList[j].Name, z)
+		// no error if exists -- will just use whichever token loaded in first
+	}
 
 	return nil
 }
