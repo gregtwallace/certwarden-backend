c131cecd2fc4a78a8a838a29ee6a06e971fbc965
Greg T. Wallace <greg@gregtwallace.com>
2023-07-03 22:19:35 -0400

    storage: fix key put sql query api_key_disabled case was wrong

--- pkg/storage/sqlite/keys_put.go
+++ pkg/storage/sqlite/keys_put.go
@@ -18,7 +18,7 @@ func (store *Storage) PutKeyUpdate(payload private_keys.UpdatePayload) (err erro
 	SET
 		name = case when $1 is null then name else $1 end,
 		description = case when $2 is null then description else $2 end,
-		api_key_disabled = case when $3 is null then description else $3 end,
+		api_key_disabled = case when $3 is null then api_key_disabled else $3 end,
 		api_key_via_url = case when $4 is null then api_key_via_url else $4 end,
 		updated_at = $5
 	WHERE
