f536300febd390df2b3082a1750b99af6d5456c2
Greg T. Wallace <greg@gregtwallace.com>
2023-08-04 18:39:49 -0400

    cloudflare: fix resource zone look up some zones can have more than two parts (e.g., some-name.in.ua) these zones were unusable due to the previous lookup method rework matching method to allow any length by trying to use known zones as suffix of resource
    Fixes https://github.com/gregtwallace/legocerthub/issues/22

--- pkg/challenges/providers/dns01cloudflare/zone.go
+++ pkg/challenges/providers/dns01cloudflare/zone.go
@@ -9,26 +9,35 @@ import (
 
 // zone stores information about a domain and is used
 // to identify which API should be used and to prevent
-// repeated unneeded ZoneID lookups view the API
+// repeated unneeded ZoneID lookups via the API
 type zone struct {
 	id  string
 	api *cloudflare.API
 }
 
-// zoneName returns the domain name with TLD from the resourceName
-func zoneName(resourceName string) (domain string) {
-	domainParts := strings.Split(resourceName, ".")
-	return domainParts[len(domainParts)-2] + "." + domainParts[len(domainParts)-1]
-}
-
 // getResourceZone returns the zone record for the specified
 // resourceName
 func (service *Service) getResourceZone(resourceName string) (zone, error) {
+	// search for a known zone as the suffix of the resource name
+	resourceZoneName := ""
+	for zoneName := range service.knownDomainZones {
+		// if resource ends in the zone name
+		if strings.HasSuffix(resourceName, zoneName) {
+			// found matching zone
+			resourceZoneName = zoneName
+			break
+		}
+	}
+
+	// if no zone found
+	if resourceZoneName == "" {
+		return zone{}, errors.New("zone name does not exist in zone map")
+	}
+
 	// get the zone related to the resourceName
-	z, exists := service.knownDomainZones[zoneName(resourceName)]
+	z, exists := service.knownDomainZones[resourceZoneName]
 	if !exists {
-		// MAYBE TODO: Check accounts again to see if zone was added. As built, LeGo will
-		// need a restart if zones are added to an account
+		// should be impossible after above name determination
 		return zone{}, errors.New("zone name does not exist in zone map")
 	}
 
