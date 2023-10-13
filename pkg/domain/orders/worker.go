9024d1192e9c378a69ba2f08092b500f27e3943d
Greg T. Wallace <greg@gregtwallace.com>
2023-10-13 19:03:23 -0400

    orders: include worker # in order job logs

--- pkg/domain/orders/worker.go
+++ pkg/domain/orders/worker.go
@@ -1,7 +1,6 @@
 package orders
 
 import (
-	"errors"
 	"legocerthub-backend/pkg/acme"
 	"net/http"
 	"sync"
@@ -27,7 +26,7 @@ doingWork:
 			// break to shutdown
 			break doingWork
 		case highJob := <-highPriorityJobs:
-			service.doOrderJob(highJob)
+			service.doOrderJob(id, highJob)
 			service.logger.Debugf("worker %d: end of high priority order fulfiller (orderId: %d)", id, highJob.orderId)
 		case lowJob := <-lowPriorityJobs:
 		lower:
@@ -37,61 +36,61 @@ doingWork:
 					// break to shutdown
 					break doingWork
 				case highJob := <-highPriorityJobs:
-					service.doOrderJob(highJob)
+					service.doOrderJob(id, highJob)
 					service.logger.Debugf("worker %d: end of high priority order fulfiller (orderId: %d)", id, highJob.orderId)
 				default:
 					break lower
 				}
 			}
-			service.doOrderJob(lowJob)
+			service.doOrderJob(id, lowJob)
 			service.logger.Debugf("worker %d: end of low priority order fulfiller (orderId: %d)", id, lowJob.orderId)
 		}
 	}
 
-	service.logger.Debugf("order worker (%d) shutdown complete", id)
+	service.logger.Debugf("worker %d: worker shutdown complete", id)
 }
 
 // doOrderJob works the orderJob specified. Once complete, the job is removed from the inProcess
 // list to indicate completion. No results are returned as results are saved directly to storage
 // as part of doing the job.
-func (service *Service) doOrderJob(job orderJob) {
+func (service *Service) doOrderJob(workerId int, job orderJob) {
 	// remove id from inProcess when goroutine is done
 	defer func(orderId int, service *Service) {
 		err := service.inProcess.remove(orderId)
 		if err != nil {
-			service.logger.Error(err)
+			service.logger.Errorf("worker %d: error: %s", workerId, err)
 		}
 	}(job.orderId, service)
 
 	// fetch the relevant order
 	orderDb, err := service.storage.GetOneOrder(job.orderId)
 	if err != nil {
-		service.logger.Error(err)
+		service.logger.Errorf("worker %d: error: %s", workerId, err)
 		return // done, failed
 	}
 
 	// always info log ordering
-	service.logger.Infof("worker ordering order id %d (certificate name: %s, subject: %s)", orderDb.ID, orderDb.Certificate.Name, orderDb.Certificate.Subject)
+	service.logger.Infof("worker %d: ordering order id %d (certificate name: %s, subject: %s)", workerId, orderDb.ID, orderDb.Certificate.Name, orderDb.Certificate.Subject)
 
 	// update certificate timestamp after fulfiller is done
 	defer func(certId int) {
 		err = service.storage.UpdateCertUpdatedTime(certId)
 		if err != nil {
-			service.logger.Error(err)
+			service.logger.Errorf("worker %d: error: %s", workerId, err)
 		}
 	}(orderDb.Certificate.ID)
 
 	// get account key
 	key, err := orderDb.Certificate.CertificateAccount.AcmeAccountKey()
 	if err != nil {
-		service.logger.Error(err)
+		service.logger.Errorf("worker %d: error: %s", workerId, err)
 		return // done, failed
 	}
 
 	// make cert CSR
 	csr, err := orderDb.Certificate.MakeCsrDer()
 	if err != nil {
-		service.logger.Error(err)
+		service.logger.Errorf("worker %d: error: %s", workerId, err)
 		return // done, failed
 	}
 
@@ -101,7 +100,7 @@ func (service *Service) doOrderJob(job orderJob) {
 	// acmeService to avoid repeated logic
 	acmeService, err := service.acmeServerService.AcmeService(orderDb.Certificate.CertificateAccount.AcmeServer.ID)
 	if err != nil {
-		service.logger.Error(err)
+		service.logger.Errorf("worker %d: error: %s", workerId, err)
 		return // done, failed
 	}
 
@@ -120,7 +119,7 @@ fulfillLoop:
 			if acmeErr, ok := err.(acme.Error); ok && acmeErr.Status == http.StatusNotFound {
 				service.storage.PutOrderInvalid(job.orderId)
 			}
-			service.logger.Error(err)
+			service.logger.Errorf("worker %d: error: %s", workerId, err)
 			return // done, failed
 		}
 
@@ -130,7 +129,7 @@ fulfillLoop:
 			var authStatus string
 			authStatus, err = service.authorizations.FulfillAuths(acmeOrder.Authorizations, key, acmeService)
 			if err != nil {
-				service.logger.Error(err)
+				service.logger.Errorf("worker %d: error: %s", workerId, err)
 				return // done, failed
 			}
 			// auth should be valid (thus making order ready)
@@ -146,14 +145,14 @@ fulfillLoop:
 			// save finalized_key_id in storage
 			err = service.storage.UpdateFinalizedKey(orderDb.ID, orderDb.Certificate.CertificateKey.ID)
 			if err != nil {
-				service.logger.Error(err)
+				service.logger.Errorf("worker %d: error: %s", workerId, err)
 				return // done, failed
 			}
 
 			// finalize the order
 			acmeOrder, err = acmeService.FinalizeOrder(acmeOrder.Finalize, csr, key)
 			if err != nil {
-				service.logger.Error(err)
+				service.logger.Errorf("worker %d: error: %s", workerId, err)
 				return // done, failed
 			}
 
@@ -171,14 +170,14 @@ fulfillLoop:
 			if acmeOrder.Certificate != nil {
 				certPemChain, err := acmeService.DownloadCertificate(*acmeOrder.Certificate, key)
 				if err != nil {
-					service.logger.Error(err)
+					service.logger.Errorf("worker %d: error: %s", workerId, err)
 					return // done, failed
 				}
 
 				// process pem and save to storage
 				err = service.savePemChain(orderDb.ID, certPemChain)
 				if err != nil {
-					service.logger.Error(err)
+					service.logger.Errorf("worker %d: error: %s", workerId, err)
 					return
 				}
 
@@ -194,7 +193,8 @@ fulfillLoop:
 				select {
 				case <-service.shutdownContext.Done():
 					// abort refreshing due to shutdown
-					service.logger.Error("order job canceled due to shutdown")
+
+					service.logger.Errorf("worker %d: order job canceled due to shutdown", workerId)
 					return
 
 				case <-time.After(time.Duration(i) * 30 * time.Second):
@@ -203,13 +203,13 @@ fulfillLoop:
 			}
 
 		case "invalid": // break, irrecoverable
-			service.logger.Infof("order status invalid; acme error: %s", acmeOrder.Error)
+			service.logger.Infof("worker %d: order status invalid; acme error: %s", workerId, acmeOrder.Error)
 			break fulfillLoop
 
 		// Note: there is no 'expired' Status case. If the order expires it simply moves to 'invalid'.
 
 		default:
-			service.logger.Error(errors.New("order status unknown"))
+			service.logger.Errorf("worker %d: error: order status unknown", workerId)
 			return // done, failed
 		}
 	}
@@ -217,7 +217,7 @@ fulfillLoop:
 	// update order in storage
 	err = service.storage.PutOrderAcme(makeUpdateOrderAcmePayload(job.orderId, acmeOrder))
 	if err != nil {
-		service.logger.Error(err)
+		service.logger.Errorf("worker %d: error: %s", workerId, err)
 	}
 
 	// if https & if order success, check if this is app's certificate and if so inform app
@@ -228,12 +228,12 @@ fulfillLoop:
 		acmeOrder.Status == "valid" {
 		err = service.loadHttpsCertificate()
 		if err != nil {
-			service.logger.Errorf("failed to load lego's new https certificate (%s)", err)
+			service.logger.Errorf("worker %d: failed to load lego's new https certificate (%s)", workerId, err)
 		} else {
-			service.logger.Debugf("new lego https certificate loaded")
+			service.logger.Debugf("worker %d: new lego https certificate loaded", workerId)
 		}
 	}
 
 	// always info log order completed
-	service.logger.Infof("worker ordering order id %d completed as %s (certificate name: %s, subject: %s)", orderDb.ID, acmeOrder.Status, orderDb.Certificate.Name, orderDb.Certificate.Subject)
+	service.logger.Infof("worker %d: order id %d completed as %s (certificate name: %s, subject: %s)", workerId, orderDb.ID, acmeOrder.Status, orderDb.Certificate.Name, orderDb.Certificate.Subject)
 }
