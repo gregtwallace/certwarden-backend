29add59828c5144209f6f47686cc0371fef1605f
Greg T. Wallace <greg@gregtwallace.com>
2023-10-26 23:37:02 -0400

    download: modify add vary header logic * multiple headers due to .Add() is fine * also make the header keys canonical

--- pkg/domain/download/header.go
+++ pkg/domain/download/header.go
@@ -8,24 +8,23 @@ import (
 // apiKeyHeaderNames are the potential header names that will be checked
 // for the api key
 var apiKeyHeaderNames = []string{
-	"X-API-Key",
 	"apiKey",
+	"X-API-Key",
 }
 
 // getApiKeyFromHeader gets the api key from approved headers and also
 // modifies ResponseWriter to include the Vary header re: api key
 func getApiKeyFromHeader(w http.ResponseWriter, r *http.Request) (apiKeyHeaderValue string) {
 	// all api key fields should be included in Vary
-	varyVals := strings.Join(apiKeyHeaderNames, ", ")
-
-	// if Vary is already set, include prior value in new value
-	existingVary := w.Header().Get("Vary")
-	if existingVary != "" {
-		varyVals += ", " + existingVary
+	varyCanonicalVals := []string{}
+	for _, varyVal := range apiKeyHeaderNames {
+		// use canonical names
+		varyCanonicalVals = append(varyCanonicalVals, http.CanonicalHeaderKey(varyVal))
 	}
+	varyValsString := strings.Join(varyCanonicalVals, ", ")
 
-	// set Vary
-	w.Header().Set("Vary", varyVals)
+	// add to Vary
+	w.Header().Add("Vary", varyValsString)
 
 	// find api key value
 	for _, headerName := range apiKeyHeaderNames {
