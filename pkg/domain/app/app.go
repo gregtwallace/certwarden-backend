dd90b2918586895bbabf03f6013c46c00190dcf2
Greg T. Wallace <greg@gregtwallace.com>
2024-03-01 16:41:33 -0500

    v0.20.1

--- pkg/domain/app/app.go
+++ pkg/domain/app/app.go
@@ -25,7 +25,7 @@ import (
 )
 
 // application version
-const appVersion = "0.20.0"
+const appVersion = "0.20.1"
 
 // config version
 // increment any time there is a breaking change between versions
