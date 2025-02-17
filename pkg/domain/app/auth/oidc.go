0832dac8f21195edd09606156d971a69c11ef709
frefrik <swift.year6073@zomg.no>
2025-02-17 14:57:31 +0100

    auth: simplify setOIDCScopes logic

--- pkg/domain/app/auth/oidc.go
+++ pkg/domain/app/auth/oidc.go
@@ -23,13 +23,14 @@ var oidcRequiredScopes []string
 
 // setOIDCScopes sets the appropriate scopes based on the issuer URL
 func setOIDCScopes(issuerURL string, appURI string) {
-	if strings.Contains(issuerURL, "login.microsoftonline.com") {
-		// For Microsoft Entra ID (Azure AD), scope needs to be in format appURI/scope
-		oidcCertWardenScope = fmt.Sprintf("%s/certwarden:superadmin", appURI)
-	} else {
-		// Default scope format for other providers
-		oidcCertWardenScope = "certwarden:superadmin"
-	}
+	// Default scope format for all providers
+	oidcCertWardenScope = "certwarden:superadmin"
+
+	// For Microsoft Entra ID, the scope format requires the full application URI prefix
+    if strings.Contains(issuerURL, "login.microsoftonline.com") && appURI != "" {
+        oidcCertWardenScope = fmt.Sprintf("%s/certwarden:superadmin", appURI)
+    }
+
 	// Set the required scopes array
 	oidcRequiredScopes = []string{oidc.ScopeOpenID, oidc.ScopeOfflineAccess, "profile", oidcCertWardenScope}
 }
