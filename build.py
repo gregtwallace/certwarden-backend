ca1eff613fc57038db235540fb83eb92948078fb
Greg T. Wallace <greg@gregtwallace.com>
2026-09-15 17:17:29 -0400

    update build for individual targets * take target as an arg * dont compress a release (main build script will do that) * updates to make exec from path agnostic (i.e., doesnt matter what path the caller is in)

--- build.py
+++ build.py
@@ -8,7 +8,9 @@ import subprocess
 import tarfile
 
 # Usage
-# python3 ./build_release.py [--gitrequired]
+# python3 ./build_release.py `target` [--gitrequired]
+
+# target should be in the form `GOOS_GOARCH` (see below).
 
 # if the gitrequired flag is used, the script will abort if it fails to get the current
 # git commit
@@ -20,16 +22,14 @@ import tarfile
 #   `GOOS_GOARCH`
 # see: https://github.com/golang/go/blob/master/src/internal/syslist/syslist.go
 # or unofficially: https://gist.github.com/asukakenji/f15ba7e588ac42795f421b48b8aede63
-targets = [
-   "windows_amd64",
-   "linux_amd64",
-   "linux_arm64",
-   "darwin_amd64",
-   "darwin_arm64",
-   "freebsd_amd64",
-   "freebsd_arm64",
-]
-
+# e.g.,
+#  "windows_amd64",
+#  "linux_amd64",
+#  "linux_arm64",
+#  "darwin_amd64",
+#  "darwin_arm64",
+#  "freebsd_amd64",
+#  "freebsd_arm64",
 
 ##
 ### Helper Functions
@@ -37,8 +37,8 @@ targets = [
 
 # Function to get current commit hash without needing git executable or lib
 # Modified version of: https://stackoverflow.com/a/68215738/7572076
-def get_commit():
-  git_folder = Path('./.git')
+def get_commit(src_path):
+  git_folder = Path(os.path.join(src_path, '.git'))
   head_content = Path(git_folder, 'HEAD').read_text().split('\n')[0]
   commit_regex = re.compile(r"^[a-fA-F0-9]{40}$")
 
@@ -70,10 +70,10 @@ path_src = Path(__file__).parents[1]
 path_root = Path(__file__).parents[2]
 
 path_output = os.path.join(path_root, "_out", "backend")
-path_release = os.path.join(path_output, "_release")
 
 # parse args
 parser = argparse.ArgumentParser()
+parser.add_argument('target')
 parser.add_argument('--gitrequired', action='store_true')
 args = parser.parse_args()
 
@@ -93,7 +93,7 @@ if versionString == "":
   exit(-1)
 
 # try to get hash
-gitHead = get_commit()
+gitHead = get_commit(path_src_backend)
 if gitHead != "":
   versionString += "_(" + gitHead[:7] + ")"
 else:
@@ -111,63 +111,46 @@ if os.path.exists(path_output):
   shutil.rmtree(path_output)
 os.makedirs(path_output)
 
-# loop through and build all targets
-for target in targets:
-  print("building certwarden-backend for target:", target, "...")
+# build target
+target = args.target
+print("building certwarden-backend for target:", target, "...")
 
-  # environment vars
-  split = target.split("_")
-  GOOS = split[0]
-  GOARCH = split[1]
-  os.environ["GOOS"] = GOOS
-  os.environ["GOARCH"] = GOARCH
-  os.environ["CGO_ENABLED"] = "0"
+# environment vars
+split = target.split("_")
+GOOS = split[0]
+GOARCH = split[1]
+os.environ["GOOS"] = GOOS
+os.environ["GOARCH"] = GOARCH
+os.environ["CGO_ENABLED"] = "0"
 
-  # send build product to GOOS_GOARCH subfolders
-  targetOutDir = os.path.join(path_output, target)
-  if not os.path.exists(targetOutDir):
-    os.makedirs(targetOutDir)
+# send build product to GOOS_GOARCH subfolders
+targetOutDir = os.path.join(path_output, target)
+if not os.path.exists(targetOutDir):
+  os.makedirs(targetOutDir)
 
-  # special case for windows to add file extensions
-  extension = ""
-  if GOOS.lower() == "windows":
-    extension = ".exe"
+# special case for windows to add file extensions
+extension = ""
+if GOOS.lower() == "windows":
+  extension = ".exe"
 
-  # build binary
-  subprocess.run(["go", "build", "-o", f"{targetOutDir}/certwarden{extension}", "./cmd/api-server"])
+# build binary
+result = subprocess.run(["go", "build", "-o", f"{targetOutDir}/certwarden{extension}", "./cmd/api-server"], cwd=path_src_backend)
+if result.returncode != 0:
+  print(f"build certwarden-backend target '{target}' failed")
+  exit(-2)
 
-  # copy other important files for release
-  shutil.copy("config.default.yaml", targetOutDir)
-  shutil.copy("config.example.yaml", targetOutDir)
-  shutil.copy("config.changelog.md", targetOutDir)
-  shutil.copy("README.md", targetOutDir)
-  shutil.copy("LICENSE.md", targetOutDir)
-  if gitHead:
-    with open(targetOutDir + "/HEAD-backend", "a") as f:
-      f.write(gitHead)
-  if GOOS.lower() == "windows":
-    shutil.copytree(os.path.join(path_src_backend, 'scripts', 'windows'), os.path.join(targetOutDir, "scripts"))
-  else:
-    shutil.copytree(os.path.join(path_src_backend, 'scripts', 'other'), os.path.join(targetOutDir, "scripts"))
-
-  # compress release file
-  # special case for windows & mac to use zip format
-  if GOOS.lower() == "windows" or GOOS.lower() == "darwin":
-    shutil.make_archive(f"{path_release}/certwarden-backend-{versionString}_{target}", "zip", targetOutDir)
-  else:
-    # for others, use gztar and set permissions on the files
-
-    # filter for setting permissions
-    def set_permissions(tarinfo):
-      if tarinfo.name == "certwarden":
-        tarinfo.mode = 0o0755
-      else:
-        tarinfo.mode = 0o0644
-      return tarinfo
-
-    # make tar
-    with tarfile.open(f"{path_release}/certwarden-{versionString}_{target}.tar.gz", "w:gz") as tar:
-        for file in os.listdir(targetOutDir):
-          tar.add(os.path.join(targetOutDir, file), arcname=file, recursive=True, filter=set_permissions)
+# copy other important files for release
+shutil.copy(os.path.join(path_src_backend, "config.default.yaml"), targetOutDir)
+shutil.copy(os.path.join(path_src_backend, "config.example.yaml"), targetOutDir)
+shutil.copy(os.path.join(path_src_backend, "config.changelog.md"), targetOutDir)
+shutil.copy(os.path.join(path_src_backend, "README.md"), targetOutDir)
+shutil.copy(os.path.join(path_src_backend, "LICENSE.md"), targetOutDir)
+if gitHead:
+  with open(targetOutDir + "/HEAD-backend", "a") as f:
+    f.write(gitHead)
+if GOOS.lower() == "windows":
+  shutil.copytree(os.path.join(path_src_backend, 'scripts', 'windows'), os.path.join(targetOutDir, "scripts"))
+else:
+  shutil.copytree(os.path.join(path_src_backend, 'scripts', 'other'), os.path.join(targetOutDir, "scripts"))
 
 print("exiting certwarden-backend build script")
